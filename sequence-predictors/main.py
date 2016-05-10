444391dbd210f69e6c08e5c2dc5a34066700f840
John Holliman <holliman@mit.edu>
2016-05-09 20:11:04 -0400

    Add experts

--- sequence-predictors/main.py
+++ sequence-predictors/main.py
@@ -1,5 +1,7 @@
 from mind_reading_machine.mind_reader import MindReader
 from seer.seer import SEER
+from experts.experts import ExpertsCombo
+from experts.experts_ema import ExpertsEMA
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,27 +15,39 @@ with open(john_data_path, 'r') as f:
 rounds = 10
 shannon_results_john = []
 hagel_results_john = []
+expert_results_john = []
+expert_ema_results_john = []
 
 for r in xrange(rounds):
     # Performance of bots
     shannon_bot = MindReader()
     hagel_bot = SEER()
+    expert_bot = ExpertsCombo([MindReader(), SEER()])
+    expert_ema_bot = ExpertsEMA([MindReader(), SEER()], 0.8)
     for human_guess in data:
         shannon_bot.take_turn(human_guess)
         hagel_bot.take_turn(human_guess)
+        expert_bot.take_turn(human_guess)
+        expert_ema_bot.take_turn(human_guess)
     shannon_results_john.append(shannon_bot.computer_score / (1.0 + len(data)))
     hagel_results_john.append(hagel_bot.computer_score / (1.0 + len(data)))
+    expert_results_john.append(expert_bot.computer_score / (1.0 + len(data)))
+    expert_ema_results_john.append(expert_ema_bot.computer_score / (1.0 + len(data)))
 
 means_john = []
 means_john.append(np.mean(shannon_results_john))
 means_john.append(np.mean(hagel_results_john))
+means_john.append(np.mean(expert_results_john))
+means_john.append(np.mean(expert_ema_results_john))
 stds_john = []
 stds_john.append(np.std(shannon_results_john))
 stds_john.append(np.std(hagel_results_john))
+stds_john.append(np.std(expert_results_john))
+stds_john.append(np.std(expert_ema_results_john))
 
 
 # Plot bar graph of bot performance
-nbots = 2
+nbots = 4
 ind = np.arange(nbots)
 bar_width = 0.5
 
@@ -53,7 +67,7 @@ axes.set_xlim([-bar_width, nbots - 1 + 2 * bar_width])
 ax.set_ylabel('Score (computer score / total predictions)')
 ax.set_title('Bot sequence prediction scores')
 ax.set_xticks(ind + bar_width / 2)
-ax.set_xticklabels(('Shannon', 'Hagelbarger'))
+ax.set_xticklabels(('Shannon', 'Hagelbarger', 'Experts Combo', 'Experts Combo (EMA)'))
 
 ax.legend((rect_john[0], ), ('John\'s Data', ))
 
